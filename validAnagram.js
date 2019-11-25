1c0ee61422d4b022111fabe89090d12cc7bc852b
Kate Pond <kate@thepondsedge.com>
2019-11-24 22:27:12 -0800

    starts validAnagram algo

---
+++ validAnagram.js
@@ -0,0 +1,13 @@
+// Given two string, write a func to determine if the second string is an anagram of the first.
+
+// Restate: Write a function that takes two strings and returns if the first string and the second string have the same characters.
+
+// Inputs: Two strings that only have lowercase alphabets
+
+// Output: A boolean
+
+function validAnagram( str1, str2) {
+ return null
+}
+
+validAnagram()
