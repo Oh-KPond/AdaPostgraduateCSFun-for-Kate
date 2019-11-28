e3cc30744628157c8dc23c808cd7e3135786d9db
Kate Pond <kate@thepondsedge.com>
2019-11-27 17:38:46 -0800

    sets up problem for countUniqueValues funtion

---
+++ countUniqueValue.js
@@ -0,0 +1,13 @@
+// Implement a function which accepts a sorted array, and counts the unique values in the array.
+
+// Restate: Create a function that takes a sorted array and returns the number of unique values that are in the array.
+
+// Input: Can be negative numbers in the array. Always sorted.
+
+// Output: An int of how many unique numbers there are in the array
+
+// Test Cases:
+// countUniqueValues([1,1,1,1,2]) => 2
+// countUniqueValues([1,2,3,4,5,12,12,13]) => 7
+// countUniqueValues([])
+
