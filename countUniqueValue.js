968974539a3b61a726614840198d3be5b3a9b0dd
Kate Pond <kate@thepondsedge.com>
2019-11-29 09:54:44 -0800

    "finishes countUniqueValue function"

--- countUniqueValue.js
+++ countUniqueValue.js
@@ -11,3 +11,23 @@
 // countUniqueValues([1,2,3,4,5,12,12,13]) => 7
 // countUniqueValues([])
 
+function countUniqueValues(arr) {
+if(arr.length == 0){
+    return console.log(0)
+}
+let pointer1 = 0
+let pointer2 = 1
+
+for(pointer2; pointer2 < arr.length; pointer2++){
+    if (arr[pointer2] != arr[pointer1]){
+        pointer1++
+        arr[pointer1] = arr[pointer2]  
+    }
+}
+
+return console.log(pointer1 + 1)
+}
+
+countUniqueValues([1,1,1,1,2])
+countUniqueValues([1,2,3,4,5,12,12,13])
+countUniqueValues([])
