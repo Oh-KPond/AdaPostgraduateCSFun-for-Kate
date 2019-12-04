7c6036b5a1c50db95e51bd2871b15c154c0205b0
Kate Pond <kate@thepondsedge.com>
2019-12-04 11:53:19 -0800

    sets up sameFreq problem

---
+++ sameFrequency.js
@@ -0,0 +1,21 @@
+// Implement a function that when given two positive integers it finds out if the two numbers have the same frequency of digits
+
+// Restate: Make a function that will take two positive numbers and return true if the numbers have the same numbers in them and false if the numbers are made up of other numbers.
+
+// Input: Positive integers only
+
+// Output: True or False
+
+// Test Cases:
+// 182, 218 => true
+// 34, 14 => false
+// 9998, 8999 => true
+// 0, 000 => false
+//3589578, 5879385 => true
+
+function sameFrequency(num1, num2){
+    
+}
+
+sameFrequency(182, 218)
+
