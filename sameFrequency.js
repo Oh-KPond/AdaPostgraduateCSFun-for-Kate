6940defe4ffe40b5600038eaf4224e7386029e76
Kate Pond <kate@thepondsedge.com>
2019-12-05 11:30:47 -0800

    completes sameFrequency with a bug.

--- sameFrequency.js
+++ sameFrequency.js
@@ -14,8 +14,47 @@
 //3589578, 5879385 => true
 
 function sameFrequency(num1, num2){
+    if(num1.length != num2.length) {
+        return console.log(false)
+    }
+
+    let charmap1 = {}
+    let charmap2 = {}
+
+    num1.toString(10).split("").forEach(num => {
+        if(num in charmap1){
+            charmap1[num] ++
+        }
+        else {
+            charmap1[num] = 1
+        }
+    });
+
+    num2.toString(10).split("").forEach (num => {
+        if(num in charmap2){
+            charmap2[num] ++
+        }
+        else {
+            charmap2[num] = 1
+        }
+    });
+
+    for(let key in charmap1) {
+        if(!(key in charmap2)) {
+            return console.log(false)
+        }
+        if(charmap1[key] != charmap2[key]){
+            return console.log(false)
+        }
+    }
     
+    return console.log(true)
 }
 
 sameFrequency(182, 218)
+sameFrequency(34, 14)
+sameFrequency(9998, 8999)
+sameFrequency(0, 000) 
+// There's a bug here to deal with. I believe that it has to do with the toString function.
+sameFrequency(3589578, 5879385)
 
